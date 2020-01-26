4340a7eb66eb3f1d2cae8f68dadd5759f24edf3b
Johannes Brandt Nielsen <johannes.brandt.nielsen@gmail.com>
2020-01-26 18:08:38 +0100

    extract useInterval from useRandomArray

--- src/Scroll/useRandomArray.js
+++ src/Scroll/useRandomArray.js
@@ -1,25 +1,23 @@
 import React from "react";
+import useInterval from "./useInterval";
 
-const buildIndexArray = length => Array.from(Array(length).keys());
-const randomInRange = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
+function buildIndexArray(length) {
+  return Array.from(Array(length).keys());
+}
 
-const useRandomArray = (time = 3000, min = 10, max = 50, enabled = true) => {
+function randomInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const useRandomArray = (delay = 3000, min = 10, max = 50, enabled = true) => {
   const [state, setState] = React.useState(
     buildIndexArray(randomInRange(min, max))
   );
 
-  React.useEffect(() => {
-    if (!enabled) {
-      return () => {};
-    }
-
-    const interval = setInterval(() => {
-      setState(buildIndexArray(randomInRange(min, max)));
-    }, time);
-
-    return () => clearInterval(interval);
-  }, [enabled, time, min, max]);
+  useInterval(
+    () => setState(buildIndexArray(randomInRange(min, max))),
+    enabled ? delay : null
+  );
 
   return state;
 };
