7f42901def74345e7e2a9234b875a601ab403751
Johannes Brandt Nielsen <johannes.brandt.nielsen@gmail.com>
2020-01-27 22:15:42 +0100

    quiet lint warning that is all ok

--- src/Scroll/useScrollRestoration.js
+++ src/Scroll/useScrollRestoration.js
@@ -1,3 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/*
+  The dependency array is built using the rest operator.
+  It wil therefore always be an array. But since it is not an
+  array literal the lint rule will complain about it
+*/
+
 import React from "react";
 
 const useScrollRestoration = (...deps) => {
