fdc26938f104cdb144e29e5f4544b60ef34c73d0
Johannes Brandt Nielsen <johannes.brandt.nielsen@gmail.com>
2020-01-28 19:14:35 +0100

    simplify scoll restoration logic

--- src/Scroll/useScrollRestoration.js
+++ src/Scroll/useScrollRestoration.js
@@ -11,19 +11,14 @@ const useScrollRestoration = (...deps) => {
   const elementRef = React.useRef();
   const savedOffset = React.useRef(0);
 
-  React.useEffect(() => {
+  if (elementRef.current) {
+    savedOffset.current = elementRef.current.offsetTop - window.scrollY;
+  }
+
+  React.useLayoutEffect(() => {
     if (savedOffset.current) {
       window.scroll(0, elementRef.current.offsetTop - savedOffset.current);
     }
-
-    const eventListener = () => {
-      savedOffset.current = elementRef.current.offsetTop - window.scrollY;
-    };
-
-    window.addEventListener("scroll", eventListener);
-    return () => {
-      window.removeEventListener("scroll", eventListener);
-    };
   }, deps);
 
   return elementRef;
